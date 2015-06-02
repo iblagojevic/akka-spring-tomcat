6cccfcf574ee8997543efc9f41ff080207ffbac5
iblagojevic <ib@digibonus.fr>
2015-06-02 11:13:32 +0200

    Create db_dump.sql

---
+++ db_dump.sql
@@ -0,0 +1,92 @@
+/*
+SQLyog Ultimate v12.03 (64 bit)
+MySQL - 5.5.41-0ubuntu0.14.04.1 : Database - akka_spring_tomcat
+*********************************************************************
+*/
+
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`currencucy_fair` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `akka_spring_tomcat`;
+
+/*Table structure for table `country` */
+
+DROP TABLE IF EXISTS `country`;
+
+CREATE TABLE `country` (
+  `id` varchar(3) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `currency` */
+
+DROP TABLE IF EXISTS `currency`;
+
+CREATE TABLE `currency` (
+  `id` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `error` */
+
+DROP TABLE IF EXISTS `error`;
+
+CREATE TABLE `error` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `message` text COLLATE utf8_unicode_ci NOT NULL,
+  `error_timestamp` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `error_user_fk` (`user_id`),
+  CONSTRAINT `error_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `transaction` */
+
+DROP TABLE IF EXISTS `transaction`;
+
+CREATE TABLE `transaction` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `country_id` varchar(3) COLLATE utf8_unicode_ci NOT NULL,
+  `currency_from_id` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+  `currency_to_id` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+  `amount_buy` double NOT NULL,
+  `amount_sell` double NOT NULL,
+  `rate` double NOT NULL,
+  `time_placed` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tx_country_fk` (`country_id`),
+  KEY `tx_currency_from_fk` (`currency_from_id`),
+  KEY `tx_currency_to_fk` (`currency_to_id`),
+  KEY `tx_user_fk` (`user_id`),
+  CONSTRAINT `tx_country_fk` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tx_currency_from_fk` FOREIGN KEY (`currency_from_id`) REFERENCES `currency` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tx_currency_to_fk` FOREIGN KEY (`currency_to_id`) REFERENCES `currency` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tx_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `bank_account_no` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=174257 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
